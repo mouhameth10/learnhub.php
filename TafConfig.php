6b3847b464391b32f97c219bc91737986e1b2435
marietawele <marietawele700@gmail.com>
2024-07-02 16:04:38 +0000

    edr

--- TafConfig.php
+++ TafConfig.php
@@ -15,9 +15,9 @@ class TafConfig
     public $database_type = "mysql"; // "mysql" | "pgsql" | "sqlsrv"
     public $host = "localhost"; // adresse ou ip du serveur
     public $port = "3306"; // 3306 pour mysql | 5432 pour pgsql | 1433 pour sqlsrv 
-    public $database_name = "c0db1"; // nom de la base de données
+    public $database_name = "c0couvoiramar"; // nom de la base de données
     public $user = "root"; // nom de l'utilisateur de la base de données
-    public $password = "root"; // mot de passe de l'utilisateur de la base de données
+    public $password = ""; // mot de passe de l'utilisateur de la base de données
 
     /* informations de connexion à la documentation */
     public $documentation_username = "admin"; // nom d'utilisateur pour accéder à la documentation
